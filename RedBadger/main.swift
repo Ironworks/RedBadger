bda9744f679180b76615f4f3508f33d0ab105c06
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2019-01-11 10:50:17 +0000

    Initial Commit

---
+++ RedBadger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RedBadger
+//
+//  Created by Trevor Doodes on 11/01/2019.
+//  Copyright © 2019 IronworksMediaLimited. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
