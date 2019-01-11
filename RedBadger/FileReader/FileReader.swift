8c69e5f2feeeeaac53259b9c2a3917be6e454d94
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2019-01-11 14:05:09 +0000

    Add FileReader initial implementation and test

--- RedBadger/FileReader/FileReader.swift
+++ RedBadger/FileReader/FileReader.swift
@@ -6,11 +6,102 @@
 //  Copyright © 2019 IronworksMediaLimited. All rights reserved.
 //
 
-//Class to read input file from bundle
+//Class to read input file from main bundle
 
 import Foundation
 
+enum Orientation {
+    case north
+    case east
+    case south
+    case west
+    
+    static func orientation(from string: String) -> Orientation {
+        
+        switch string {
+        case "N":
+            return Orientation.north
+        case "E":
+            return Orientation.east
+        case "S":
+            return Orientation.south
+        case "W":
+            return Orientation.west
+        default:
+            return Orientation.north
+        }
+    }
+}
+
+struct RobotCommand {
+    let startPoint: CGPoint
+    let startOrientation: Orientation
+    let moves: [String]
+}
+
+struct Commands {
+    let gridSize: CGPoint
+    let robotCommands: [RobotCommand]
+}
+
 class FileReader {
     
+    let fileName: String
+    let fileType: String
     
+    init(fileName: String, type: String) {
+        self.fileName = fileName
+        self.fileType = type
+    }
+    
+    func readFile() -> Commands? {
+        let bundle  = Bundle(for: type(of: self))
+        
+        
+        guard let url = bundle.url(forResource: fileName, withExtension: fileType) else {
+            return nil
+        }
+        
+        let commandString = try? String(contentsOf: url)
+        let commandsArray = commandString?.split(separator: "\n")
+        
+        let gridString = String(commandsArray![0])
+        let grid = point(from: gridString)
+        
+        var robotCommands: [RobotCommand] = []
+        var directions: [String]
+        var startPoint = CGPoint(x: 0, y: 0)
+        var startOrientation = Orientation.north
+        for i in 1...(commandsArray?.count)! - 1 {
+
+            if i % 2 == 0 {
+                //Even so must be directions
+                directions = String(commandsArray![i]).map { String ($0) }
+                let robotCommand = RobotCommand(startPoint: startPoint, startOrientation: startOrientation, moves: directions)
+                robotCommands.append(robotCommand)
+            } else {
+                let s = String(commandsArray![i]).split(separator: " ")
+                startPoint = point(from: s[0] + " " + s[1])
+                startOrientation = Orientation.orientation(from: String(s[2]))
+            }
+        }
+        
+        return Commands(gridSize: grid, robotCommands: robotCommands)
+        
+    }
+    
+    private func point(from string: String) -> CGPoint {
+        
+        let gridSizeArray = string.split(separator: " ")
+        
+        guard let x = Int(String(gridSizeArray[0])),
+            let y = Int(String(gridSizeArray[1])) else {
+                return CGPoint(x: 0, y: 0)
+        }
+        
+        return CGPoint(x: x, y: y)
+        
+    }
+    
+
 }
--- RedBadgerTests/FileReaderTests.swift
+++ RedBadgerTests/FileReaderTests.swift
@@ -15,7 +15,7 @@ class FileReaderTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        sut = FileReader()
+        sut = FileReader(fileName: "test", type: "txt")
     }
 
     override func tearDown() {
@@ -25,5 +25,58 @@ class FileReaderTests: XCTestCase {
 
     func testCanCreateFileReader () {
         XCTAssertNotNil(sut, "Should be able to create FileReader")
+    }
+    
+    func testCanSetFileNameAndType() {
+        XCTAssert(sut.fileName == "test", "Should be able to set file name")
+        XCTAssert(sut.fileType == "txt", "Should be able to set file type")
+    }
+    
+    func testCanReadFile() {
+        let commands = sut.readFile()
+        
+        XCTAssert(commands?.gridSize == CGPoint(x: 5, y: 3), "Should set grid size correctly")
+        XCTAssert(commands?.robotCommands.count == 3, "Should have 3 sets of robot commands")
+        
+        
+        XCTAssert(commands?.robotCommands[0].startPoint == CGPoint(x: 1, y: 1))
+        XCTAssert(commands?.robotCommands[0].startOrientation == .east, "Should set start orientation correctly")
+        XCTAssert(commands?.robotCommands[0].moves[0] == "R", "Should get correct first move")
+        XCTAssert(commands?.robotCommands[0].moves[1] == "F", "Should get correct second move")
+        XCTAssert(commands?.robotCommands[0].moves[2] == "R", "Should get correct third move")
+        XCTAssert(commands?.robotCommands[0].moves[3] == "F", "Should get correct forth move")
+        XCTAssert(commands?.robotCommands[0].moves[4] == "R", "Should get correct fifth move")
+        XCTAssert(commands?.robotCommands[0].moves[5] == "F", "Should get correct sixth move")
+        XCTAssert(commands?.robotCommands[0].moves[6] == "R", "Should get correct seventh move")
+        XCTAssert(commands?.robotCommands[0].moves[7] == "F", "Should get correct eigth move")
+        
+        XCTAssert(commands?.robotCommands[1].startPoint == CGPoint(x: 3, y: 2))
+        XCTAssert(commands?.robotCommands[1].startOrientation == .north, "Should set start orientation correctly")
+        XCTAssert(commands?.robotCommands[1].moves[0] == "F", "Should get correct first move")
+        XCTAssert(commands?.robotCommands[1].moves[1] == "R", "Should get correct second move")
+        XCTAssert(commands?.robotCommands[1].moves[2] == "R", "Should get correct third move")
+        XCTAssert(commands?.robotCommands[1].moves[3] == "F", "Should get correct forth move")
+        XCTAssert(commands?.robotCommands[1].moves[4] == "L", "Should get correct fifth move")
+        XCTAssert(commands?.robotCommands[1].moves[5] == "L", "Should get correct sixth move")
+        XCTAssert(commands?.robotCommands[1].moves[6] == "F", "Should get correct seventh move")
+        XCTAssert(commands?.robotCommands[1].moves[7] == "F", "Should get correct eigth move")
+        XCTAssert(commands?.robotCommands[1].moves[8] == "R", "Should get correct ninth move")
+        XCTAssert(commands?.robotCommands[1].moves[9] == "R", "Should get correct tenth move")
+        XCTAssert(commands?.robotCommands[1].moves[10] == "F", "Should get correct eleventh move")
+        XCTAssert(commands?.robotCommands[1].moves[11] == "L", "Should get correct twelveth move")
+        XCTAssert(commands?.robotCommands[1].moves[12] == "L", "Should get correct thirteenth move")
+        
+        XCTAssert(commands?.robotCommands[2].startPoint == CGPoint(x: 0, y: 3))
+        XCTAssert(commands?.robotCommands[2].startOrientation == .west, "Should set start orientation correctly")
+        XCTAssert(commands?.robotCommands[2].moves[0] == "L", "Should get correct first move")
+        XCTAssert(commands?.robotCommands[2].moves[1] == "L", "Should get correct second move")
+        XCTAssert(commands?.robotCommands[2].moves[2] == "F", "Should get correct third move")
+        XCTAssert(commands?.robotCommands[2].moves[3] == "F", "Should get correct forth move")
+        XCTAssert(commands?.robotCommands[2].moves[4] == "F", "Should get correct fifth move")
+        XCTAssert(commands?.robotCommands[2].moves[5] == "L", "Should get correct sixth move")
+        XCTAssert(commands?.robotCommands[2].moves[6] == "F", "Should get correct seventh move")
+        XCTAssert(commands?.robotCommands[2].moves[7] == "L", "Should get correct eigth move")
+        XCTAssert(commands?.robotCommands[2].moves[8] == "F", "Should get correct ninth move")
+        XCTAssert(commands?.robotCommands[2].moves[9] == "L", "Should get correct tenth move")
     }
 }
