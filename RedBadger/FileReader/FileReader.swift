da7508bd102cf6ed30bffb8d35c5792338f111e7
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2019-01-11 11:22:51 +0000

    Add FileReader class

---
+++ RedBadger/FileReader/FileReader.swift
@@ -0,0 +1,16 @@
+//
+//  FileReader.swift
+//  RedBadger
+//
+//  Created by Trevor Doodes on 11/01/2019.
+//  Copyright © 2019 IronworksMediaLimited. All rights reserved.
+//
+
+//Class to read input file from bundle
+
+import Foundation
+
+class FileReader {
+    
+    
+}
--- RedBadgerTests/FileReaderTests.swift
+++ RedBadgerTests/FileReaderTests.swift
@@ -7,15 +7,23 @@
 //
 
 import XCTest
+@testable import RedBadger
 
 class FileReaderTests: XCTestCase {
+    
+    var sut: FileReader!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = FileReader()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
+    func testCanCreateFileReader () {
+        XCTAssertNotNil(sut, "Should be able to create FileReader")
+    }
 }
