11a3fdc43a329210410e5b43e80b93d193c4f764
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2019-01-11 15:20:40 +0000

    Adds MarsTests Class

---
+++ RedBadgerTests/MarsTests.swift
@@ -0,0 +1,21 @@
+//
+//  MarsTests.swift
+//  RedBadgerTests
+//
+//  Created by Trevor Doodes on 11/01/2019.
+//  Copyright © 2019 IronworksMediaLimited. All rights reserved.
+//
+
+import XCTest
+
+class MarsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
