30b3db8e883fc1f7a61f6164bd5678f2b980687c
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2019-01-11 16:59:13 +0000

    Adds RobotTests

---
+++ RedBadgerTests/RobotTests.swift
@@ -0,0 +1,22 @@
+//
+//  RobotTests.swift
+//  RedBadgerTests
+//
+//  Created by Trevor Doodes on 11/01/2019.
+//  Copyright © 2019 IronworksMediaLimited. All rights reserved.
+//
+
+import XCTest
+
+class RobotTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+}
