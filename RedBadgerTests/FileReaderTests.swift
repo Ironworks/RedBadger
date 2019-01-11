8f4b98d2056c25b5d4e01058e6035d02b4efd232
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2019-01-11 11:05:54 +0000

    Adds FileReaderTests to project.

---
+++ RedBadgerTests/FileReaderTests.swift
@@ -0,0 +1,21 @@
+//
+//  FileReaderTests.swift
+//  RedBadgerTests
+//
+//  Created by Trevor Doodes on 11/01/2019.
+//  Copyright © 2019 IronworksMediaLimited. All rights reserved.
+//
+
+import XCTest
+
+class FileReaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
