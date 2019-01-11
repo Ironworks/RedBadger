df6ce470a8faff8a823508440dde5a855c5e1fe5
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2019-01-11 16:47:01 +0000

    Adds Mars class Adds Tests for Mars class

---
+++ RedBadger/Models/Mars.swift
@@ -0,0 +1,61 @@
+//
+//  Mars.swift
+//  RedBadger
+//
+//  Created by Trevor Doodes on 11/01/2019.
+//  Copyright © 2019 IronworksMediaLimited. All rights reserved.
+//
+
+import Foundation
+
+protocol Planet {
+    func locationExists(at point: CGPoint) -> Bool
+    func setRobotLost(at point: CGPoint)
+}
+
+class Mars: Planet {
+    
+    struct GridLocation {
+        var robotLost: Bool
+    }
+    
+    var topRight: CGPoint
+    var grid: [[GridLocation]]
+    
+    init(topRight: CGPoint) {
+        
+        self.topRight = topRight
+        grid = [[]]
+        
+        for _ in 0...Int(topRight.x) {
+            var yAxis: [GridLocation] = []
+            for _ in 0...Int(topRight.y) {
+                yAxis.append(GridLocation(robotLost: false))
+            }
+            grid.append(yAxis)
+        }
+        grid.remove(at: 0) //Need to remove empty row created at initialisation.
+    }
+    
+    func setRobotLost(at point: CGPoint) {
+        //Check location exists
+        if locationExists(at: point) &&
+           locationIsEdge(at: point) {
+            grid[Int(point.x)][Int(point.y)].robotLost = true
+        }
+    }
+    
+    func locationExists(at point: CGPoint) -> Bool {
+        return point.x >= 0 &&
+               point.x <= topRight.x &&
+               point.y >= 0 &&
+               point.y <= topRight.y
+    }
+    
+    private func locationIsEdge(at point: CGPoint) -> Bool {
+        return point.x == 0 ||
+               point.x == topRight.x ||
+               point.y == 0 ||
+               point.y == topRight.y
+    }
+}
--- RedBadgerTests/MarsTests.swift
+++ RedBadgerTests/MarsTests.swift
@@ -7,15 +7,51 @@
 //
 
 import XCTest
+@testable import RedBadger
 
 class MarsTests: XCTestCase {
+    
+    var sut: Mars!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = Mars(topRight: CGPoint(x: 5, y: 5))
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+    
+    func testCanCreateMars() {
+        XCTAssertNotNil(sut, "Should be able to create mars")
+        XCTAssert(sut.topRight == CGPoint(x: 5, y: 5))
+        XCTAssert(sut.grid.count == 6, "Should have 6 rows")
+        XCTAssert(sut.grid[0].count == 6, "Should have 6 columns")
+        
+        for i in 0...sut.grid.count - 1 {
+            for j in 0...sut.grid[i].count - 1 {
+                XCTAssertFalse(sut.grid[i][j].robotLost, "Should set robot lost to false")
+            }
+        }
+    }
+    
+    func testLocationExists () {
+        XCTAssertTrue(sut.locationExists(at: CGPoint(x: 3, y: 3)), "Should return true for a valid location")
+    }
+    
+    func testLocationOutOfBounds () {
+        XCTAssertFalse(sut.locationExists(at: CGPoint(x: 7, y: 7)), "Should return false for a valid location")
+    }
+    
+    func testCanSetRobotLostAtValidLocation () {
+        sut.setRobotLost(at: CGPoint(x: 5, y: 3))
+        XCTAssertTrue(sut.grid[5][3].robotLost, "Should set robot lost to true")
+    }
+    
+    func testCantSetRobotLostIfNotAtEdgeOfPlanet () {
+        sut.setRobotLost(at: CGPoint(x: 3, y: 3))
+        XCTAssertFalse(sut.grid[3][3].robotLost, "Should not set robot lost if location is not at the edge of the grid")
     }
 
 }
