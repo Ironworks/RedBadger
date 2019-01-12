01237a05e004666a2a194852e99ea6a8fa171bd3
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2019-01-12 01:26:13 +0000

    Implements main program code.

--- RedBadger/main.swift
+++ RedBadger/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+func processCommands(commands: Commands) {
+    //Create Mars
+    let planet = Mars(topRight: commands.gridSize)
+    
+    for robotCommand in commands.robotCommands {
+        //Create a robot
+        let robot = Robot(planet: planet, position: robotCommand.startPoint, orientation: robotCommand.startOrientation)
+        for instruction in robotCommand.moves {
+            robot.execute(instruction: Instruction.instructionFromString(string: instruction))
+        }
+        print (robot.status())
+    }
+}
+
+
+//Create fileReader
+let fileReader = FileReader(fileName: "input", type: "txt")
+
+//Initialise commands
+var commands: Commands
+
+//Read file
+do {
+    commands = try fileReader.readFile()
+    processCommands(commands: commands)
+
+} catch let error {
+    //In the event of failure print error
+    print(error)
+}
+
+
+
+
 
